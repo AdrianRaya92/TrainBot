9a8a6e303be96baef9e23d2e95e3c2f397cfcd5c
Adrian Raya <adrianraya@gmail.com>
2021-07-21 17:54:34 +0200

    Updated

--- app/src/main/java/com/example/trainbot/Desarrollador.kt
+++ app/src/main/java/com/example/trainbot/Desarrollador.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_desarrollador.*
-import kotlinx.android.synthetic.main.activity_generador_trainer.*
 
 class Desarrollador : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/trainbot/TablaEjercicios.kt
@@ -0,0 +1,11 @@
+package com.example.trainbot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TablaEjercicios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabla_ejercicios)
+    }
+}
